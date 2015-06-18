b672cbb865c7b3ea00e824c9dc5d2b0439246ca9
BJ Miller <bjmiller@tableausoftware.com>
2015-06-18 14:32:27 -0700

    Quick refactor for FF compatimility

--- components/reveal/reveal.js
+++ components/reveal/reveal.js
@@ -2,15 +2,15 @@
  * Reveal content utility
  */
 (function($){
+  var $triggers = $('.reveal__trigger'),
+      $contents = $('.reveal__content'),
+      animation = {
+        duration: 1000,
+        easing: "easeInOutQuart"
+      };
+  
   $(document).ready(function(){
-    if ($('.reveal__trigger').length && $('.reveal__content').length) {
-      var $triggers = $('.reveal__trigger'),
-          $contents = $('.reveal__content'),
-          animation = {
-            duration: 1000,
-            easing: "easeInOutQuart"
-          };
-
+    if ($triggers.length && $contents.length) {
       // Run setup
       setup();
 
@@ -22,77 +22,76 @@
         } else if (state == 'open') {
           hideContent(this);
         }
-        
         e.preventDefault();
       });
 
       $('.reveal__close').click(function(e) {
         $(this).parent('.reveal__content').data('revealTrigger').click();
-
         e.preventDefault();
       });
-
-      // Show the target content
-      function showContent(trigger) {
-        var data = $(trigger).data(),
-            $target = $('#' + data.revealTarget),
-            $curtain = $('#' + data.revealCurtain),
-            type = data.revealType;
-
-        $(trigger).data('revealState', 'open');
-        $target.slideDown(animation);
-        $curtain.slideUp(animation);
-
-        if (type == "video") {
-          setTimeout(function() {
-            $target.find('video')[0].play();
-          }, animation.duration/2);
-        }
-      }
-
-      // Hide the target content
-      function hideContent(trigger) {
-        var data = $(trigger).data(),
-            $target = $('#' + data.revealTarget),
-            $curtain = $('#' + data.revealCurtain),
-            type = data.revealType;
-
-        $(trigger).data('revealState', 'closed');
-        $target.slideUp(animation);
-        $curtain.slideDown(animation);
-
-        if (type == "video") {
-          $target.find('video')[0].pause();
-        }
-      }
-
-      // Hand-full of setup tasks
-      function setup() {
-        // Add reveal-state data
-        $triggers.data('revealState', 'closed');
-        
-        // Link content back to it's corresponding trigger
-        $triggers.each(function(index, el) {
-          var $target = $('#' + $(this).data('revealTarget'));
-          $target.data('revealTrigger', $(this));
-        });
-
-        // // Set initial margin on content if there is a curtain
-        // // @TODO this is for naimating the reveal as if the content is 
-        // // stationary and the elements above and below are revealing it. 
-        // // Currently, the content moves up as the curtain slides up.
-        // $contents.each(function(index, el) {
-        //   var data = $($(this).data('revealTrigger')).data(),
-        //       $curtain = $("#" + data.revealCurtain);
-
-        //   if ($curtain.length) {
-        //     $(this).css('margin-top', -$curtain.outerHeight(true));
-        //   }
-        // });
-
-        // Add a close icon to each content continer
-        $contents.prepend($('<a href="#" class="reveal__close" href="#">&#9587;</a>'));
-      }
     }
   });
+
+  // Show the target content
+  function showContent(trigger) {
+    var data = $(trigger).data(),
+        $target = $('#' + data.revealTarget),
+        $curtain = $('#' + data.revealCurtain),
+        type = data.revealType;
+
+    $(trigger).data('revealState', 'open');
+    $target.slideDown(animation);
+    $curtain.slideUp(animation);
+
+    if (type == "video") {
+      setTimeout(function() {
+        $target.find('video')[0].play();
+      }, animation.duration/2);
+    }
+  }
+
+  // Hide the target content
+  function hideContent(trigger) {
+    var data = $(trigger).data(),
+        $target = $('#' + data.revealTarget),
+        $curtain = $('#' + data.revealCurtain),
+        type = data.revealType;
+
+    $(trigger).data('revealState', 'closed');
+    $target.slideUp(animation);
+    $curtain.slideDown(animation);
+
+    if (type == "video") {
+      $target.find('video')[0].pause();
+    }
+  }
+
+  // Hand-full of setup tasks
+  function setup() {
+    // Add reveal-state data
+    $triggers.data('revealState', 'closed');
+    
+    // Link content back to it's corresponding trigger
+    $triggers.each(function(index, el) {
+      var $target = $('#' + $(this).data('revealTarget'));
+      $target.data('revealTrigger', $(this));
+    });
+
+    // // Set initial margin on content if there is a curtain
+    // // @TODO this is for naimating the reveal as if the content is 
+    // // stationary and the elements above and below are revealing it. 
+    // // Currently, the content moves up as the curtain slides up.
+    // $contents.each(function(index, el) {
+    //   var data = $($(this).data('revealTrigger')).data(),
+    //       $curtain = $("#" + data.revealCurtain);
+
+    //   if ($curtain.length) {
+    //     $(this).css('margin-top', -$curtain.outerHeight(true));
+    //   }
+    // });
+
+    // Add a close icon to each content continer
+    $contents.prepend($('<a href="#" class="reveal__close" href="#">&#9587;</a>'));
+  }
+
 })(jQuery);
