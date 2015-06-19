c5ab33fc9adbebcf31b7ecf490de2be4f60bdcf2
BJ Miller <bjmiller@tableausoftware.com>
2015-06-19 16:30:29 -0700

    Building out viz slideshow componetn

---
+++ components/viz_slideshow/viz-slideshow.js
@@ -0,0 +1,17 @@
+(function($){
+  var $vizSlideshow = $('.viz-slideshow__slides');
+
+  $(document).ready(function(){
+    if ($vizSlideshow.length) {
+      $vizSlideshow.slick({
+        centerMode: true,
+        centerPadding: '200px',
+        slidesToShow: 1,
+        arrows: true,
+        slide: '.viz-slideshow__slide',
+        prevArrow: $(this).find('.viz-slideshow__arrow--prev'),
+        nextArrow: $(this).find('.viz-slideshow__arrow--next')
+      });
+    }
+  });
+})(jQuery);
