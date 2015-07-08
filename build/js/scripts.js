114402cec14a5b5c799c71664abda8985c70926d
BJ Miller <bjmiller@tableausoftware.com>
2015-07-08 10:44:30 -0700

    adding more comments to gif-player

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -282,7 +282,10 @@ function dataSourcesSearch() {
         $gif.data('static-src', $gif.attr('src'));
 
         // Lazy load in gifs so they start animating after brought into view.
-        // Switch back to placeholder whne exited view
+        // Switch back to placeholder when image has exited view.
+        // 
+        // @todo store gif length in a data param and indicate when the gif is
+        // being animated vs static. Add a replay button once the loop ends
         var inview = new Waypoint.Inview({
           element: $gif[0],
           entered: function(direction) {
