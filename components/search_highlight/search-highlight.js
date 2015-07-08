f507eaaef59b5fb70f81f268059073519c754151
BJ Miller <bjmiller@tableausoftware.com>
2015-07-08 13:56:39 -0700

    Adding search highlight component

---
+++ components/search_highlight/search-highlight.js
@@ -0,0 +1,31 @@
+/** 
+ * Search Highlight utility.
+ *
+ * Searches through a list of items and highlights items that match the term.
+ */
+(function($){
+  var $searches = $('.search-highlight');
+  
+  $(document).ready(function(){
+    if ($searches.length) {
+      $searches.each(function(index, el) {
+        var $search = $(el),
+            $content = $('#' + $search.data('content')),
+            highlightClass = $search.data('highlight-class') + " search-highlight__match",
+            $contentItems = $content.find('li');
+
+        $search.on('change paste keyup search', function(e) {
+          var term = $(this).val().toLowerCase();
+          $contentItems.each(function(index, item) {
+            var text = $(item).text().toLowerCase();
+            $(item).removeClass(highlightClass);
+            if (term.length > 0 && text.indexOf(term) > -1) {
+              $(item).addClass(highlightClass);
+            }
+          });
+        });
+
+      });
+    }
+  });
+})(jQuery);
