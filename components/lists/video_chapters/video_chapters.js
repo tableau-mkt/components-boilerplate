eebaf914c0496d9fa7abd77fe7f628395f7b77c0
Gary Gao <ggao@tableausoftware.com>
2015-09-25 18:56:48 -0700

    Add video_chapters component set

---
+++ components/lists/video_chapters/video_chapters.js
@@ -0,0 +1,55 @@
+/**
+ * Brightcove video chapter handling.
+ *
+ * This handles chaptering interaction given an expected DOM structure. E.g.:
+ * <ul class="video__chapters" data-for="[VIDEO DOM ID]">
+ *   <li class="video__chapter" data-timestamp="60">Something</li>
+ *   <li class="video__chapter" data-timestamp="120">Something else</li>
+ * </ul>
+ *
+ * It listens for a brightcove:ready event that is raised per video instance as
+ * it successfully creates a Brightcove videojs wrapped player object.
+ */
+(function ($, window) {
+  $(document).ready(function () {
+    var $chapterLists = $('.video__chapters');
+
+    // Bail early if there aren't even any lists of chapters.
+    if (!$chapterLists.length || !typeof window.videojs === 'function') {
+      return;
+    }
+
+    // The Brightcove player binding is async. We wait for a raised event first
+    // before binding the video chapter actions.
+    $(document).bind('brightcove:ready', function (e, data) {
+      // The 'data' received here is the id attribute of the video player element.
+      var $readyChapters = $chapterLists.filter('[data-for="' + data + '"]'),
+          $videoElement = $('#' + data),
+          BCPlayer = $videoElement.data('bcPlayer');
+
+      // Bail early.
+      if (!$readyChapters.length) {
+        return;
+      }
+
+      $readyChapters.find('.video__chapter').on('click.chapter', function triggerVideoChapter (e) {
+        var $this = $(this),
+            timestamp = $this.data('timestamp');
+
+        e.preventDefault();
+
+        // Set the play time.
+        BCPlayer.currentTime(timestamp);
+
+        // Scroll.
+        smoothScrollTop($videoElement);
+
+        // Play the video if it ain't playing.
+        if (BCPlayer.paused()) {
+          BCPlayer.play();
+        }
+      })
+    });
+
+  });
+})(jQuery, window);
