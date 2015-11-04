cba8df9ef673594d0aac70fd6c639f1f999f516e
Etroid <mkeereman@tableau.com>
2015-11-03 16:04:18 -0800

    Global nav fixes.

--- components/navigation/global_nav/global-nav.js
+++ components/navigation/global_nav/global-nav.js
@@ -2,20 +2,29 @@
  * Global gavigation interactions
  */
 (function($){
-  var $globalNav = $('.global-nav__top'),
+  /**
+   * Wait for custom event 'tabAjaxMegaMenu:ready' (i.e. menu drawers are fully loaded
+   * via AJAX callback) before initiating mega menu client side behavior. If the drawers
+   * are not being loaded via AJAX, the following snippet can be used to call the trigger
+   *
+   * $(document).ready(function() {
+   *    $(document).trigger('tabAjaxMegaMenu:ready');
+   * });
+   *
+   */
+  $(document).one('tabAjaxMegaMenu:ready', function tabAjaxMenuReady() {
+    var $globalNav = $('.global-nav__top'),
       $menu = $globalNav.find('.global-nav__primary-menu'),
-      $expandableLinks = $menu.find('li.expandable'),
+      $expandableLinks = $menu.find('li a.expandable'),
       $drawers = $('.global-nav__drawers__drawer'),
       $hamburger = $globalNav.find('.hamburger'),
       $mobileWrapper = $globalNav.find('.global-nav__mobile-wrapper'),
       $mobileDrawerClose = $('.global-nav__drawers__drawer__close'),
       animation = {
         duration: 750,
-        easing: "easeInOutQuart"
+        easing: "linear"
       };
 
-  $(document).ready(function(){
-    
     /* Desktop stuff */
     if (matchMedia('(min-width: 961px)').matches) {
       // Drawer Expanding interaction
@@ -29,9 +38,9 @@
         function() {
           throttle($(this));
         }, function() {
-          var $link = $(this);
+          var $link = $(this),
               $hoverElements = $globalNav.closest('.global-nav').siblings(),
-              $navLinks = $globalNav.find('a').not($link.find('a'));
+              $navLinks = $globalNav.find('a').not($link);
 
           $hoverElements.add($navLinks).hover(function() {
             closeDrawer($link);
@@ -48,7 +57,7 @@
     if (matchMedia('(max-width: 960px)').matches) {
 
       // Set the height of the dropdown content
-      mobileHeightAdjust()
+      mobileHeightAdjust();
       
       $(window).resize(function(e) {
         mobileHeightAdjust()
@@ -95,55 +104,54 @@
       e.preventDefault();
     });
 
+    function openDrawer($link) {
+      var $drawer = $drawers.filter('#' + $link.data('drawer-id'));
+
+      if (!$drawers.hasClass('expanded')) {
+        $link.add($drawer).addClass('expanded');
+        $drawers.filter('#' + $link.data('drawer-id')).slideDown(animation);
+      }
+    }
+
+    function closeDrawer($link) {
+      var $drawer = $drawers.filter('#' + $link.data('drawer-id'));
+
+      if ($drawer.hasClass('expanded')) {
+        $link.add($drawer).removeClass('expanded');
+        $drawers.filter('#' + $link.data('drawer-id')).slideUp(animation);
+      }
+    }
+
+    function closeDrawerMobile($drawer) {
+      $drawer.add($mobileWrapper).animate({
+        marginLeft: '+=100%'
+      }, animation);
+
+      setTimeout(function() {
+        $drawer.hide().removeClass('open');
+      }, animation.duration);
+    }
+
+    function mobileHeightAdjust() {
+      // @todo this is pretty bad... Can probably figure out a clever CSS hack to
+      // acheive this with vh units or something.
+      var drawerHeight = $(window).outerHeight(true) - $globalNav.outerHeight(true);
+
+      $mobileWrapper.add($drawers).each(function(index, el) {
+        var $wrapper = $(el),
+          origHeight = $wrapper.data('orig-height');
+
+        if (isNaN(origHeight)) {
+          origHeight = $wrapper.height();
+          $wrapper.data('orig-height', origHeight);
+        }
+
+        if (origHeight < drawerHeight) {
+          $wrapper.height(drawerHeight);
+        }
+      });
+    }
 
   });
-
-  function openDrawer($link) {
-    var $drawer = $drawers.filter('#' + $link.data('drawer-id'));
-
-    if (!$drawers.hasClass('expanded')) {
-      $link.add($drawer).addClass('expanded');
-      $drawers.filter('#' + $link.data('drawer-id')).slideDown(animation);
-    }
-  }
-
-  function closeDrawer($link) {
-    var $drawer = $drawers.filter('#' + $link.data('drawer-id'));
-
-    if ($drawer.hasClass('expanded')) {
-      $link.add($drawer).removeClass('expanded');
-      $drawers.filter('#' + $link.data('drawer-id')).slideUp(animation);
-    }
-  }
-
-  function closeDrawerMobile($drawer) {
-    $drawer.add($mobileWrapper).animate({
-      marginLeft: '+=100%'
-    }, animation);
-
-    setTimeout(function() {
-      $drawer.hide().removeClass('open');
-    }, animation.duration);
-  }
-
-  function mobileHeightAdjust() {
-    // @todo this is pretty bad... Can probably figure out a clever CSS hack to 
-    // acheive this with vh units or something.
-    var drawerHeight = $(window).outerHeight(true) - $globalNav.outerHeight(true);
-
-    $mobileWrapper.add($drawers).each(function(index, el) {
-      var $wrapper = $(el),
-          origHeight = $wrapper.data('orig-height');
-      
-      if (isNaN(origHeight)) {
-        origHeight = $wrapper.height();
-        $wrapper.data('orig-height', origHeight);
-      }
-
-      if (origHeight < drawerHeight) {
-        $wrapper.height(drawerHeight);
-      }
-    });
-  }
 
 })(jQuery);
