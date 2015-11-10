b160674c86aef9289cf2875eb87298461b8cf29b
Joel Walters <jwalters@tableau.com>
2015-11-09 22:13:05 -0800

    Refine section search and add some basic behaviors.
    Revert changes to icons
    
    Some final tweaks for contextual-search component.

---
+++ components/forms/contextual_search/contextual-search.styleguide.js
@@ -0,0 +1,20 @@
+/**
+ * Section search styleguide integration.
+ */
+
+jQuery(function ready($) {
+  $('.contextual-search__input').keydown(function (event) {
+    var keyCode = $.ui.keyCode;
+
+    switch (event.keyCode) {
+      case keyCode.UP:
+      case keyCode.DOWN:
+      case keyCode.ESCAPE:
+      case keyCode.ENTER:
+        break;
+      default:
+        $(this).parents('.contextual-search').addClass('is-open');
+    }
+
+  });
+});
