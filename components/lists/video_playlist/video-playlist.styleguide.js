1370528a4d9f1c3936de8cce5b182d53a3da180d
Etroid <mkeereman@tableau.com>
2015-11-16 12:58:48 -0800

    Review feedback.

--- components/lists/video_playlist/video-playlist.styleguide.js
+++ components/lists/video_playlist/video-playlist.styleguide.js
@@ -5,11 +5,11 @@
   $(document).ready(function () {
     var $playlistVideo = $('.video-playlist__link');
 
-    $playlistVideo.click(function(e) {
+    $playlistVideo.click(function playlistVideoClick(e) {
       e.preventDefault();
 
-      $('.video-playlist__item-wrapper.active').removeClass('active');
-      $(this).closest('.video-playlist__item-wrapper').addClass('active');
+      $('.video-playlist__item-wrapper.is-active').removeClass('is-active');
+      $(this).closest('.video-playlist__item-wrapper').addClass('is-active');
     });
 
   });
