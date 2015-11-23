f18de52d8904f584bf76bb7d0c4ef4416d1218bb
Gary Gao <ggao@tableausoftware.com>
2015-11-22 19:25:57 -0800

    Move globa-nav search styleguide specific js

--- components/navigation/global_nav/global-nav.styleguide.js
+++ components/navigation/global_nav/global-nav.styleguide.js
@@ -1,5 +1,29 @@
 // Fake the 'tabAjaxMegaMenu:ready' event in order to demonstrate the navigation
 // within the styleguide.
 $(document).ready(function() {
+  var $search = $('.global-nav__search'),
+      availableTags = [
+        "Tableau",
+        "Desktop",
+        "Server",
+        "Online",
+        "Cloud",
+        "Public",
+        "Reader",
+        "Business Intelligence",
+        "Data",
+        "Products",
+        "Graphs",
+        "Visualizations"
+      ];
+
   $(document).trigger('tabAjaxMegaMenu:ready');
+
+  // Search auto-complete for demo purposes. Requires jQuery UI Autocomplete
+  // @todo add support for highlighting the searched characters in the list
+  //    http://stackoverflow.com/questions/2435964/jqueryui-how-can-i-custom-format-the-autocomplete-plug-in-results
+  $search.find("input").autocomplete({
+    source: availableTags,
+    appendTo: ".global-nav"
+  });
 });
