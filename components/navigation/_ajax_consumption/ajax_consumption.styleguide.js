54f98662531c4f4326892f26535aa88fd365fa86
Etroid <mkeereman@tableau.com>
2015-11-30 11:40:55 -0800

    Simplify styleguide js for demonstration purposes.

--- components/navigation/_ajax_consumption/ajax_consumption.styleguide.js
+++ components/navigation/_ajax_consumption/ajax_consumption.styleguide.js
@@ -1,36 +1,7 @@
 (function($) {
   $(document).ready(function() {
-    var domain = 'redesign-tableau.gotpantheon.com',//replace with cdn.tableau.com
-        lang = $('html').attr('lang') || 'en',
-        lang_map = {
-          'en' : '',
-          'fr' : 'fr-fr/',
-          'de' : 'de-de/',
-          'pt-br' : 'pt-br/',
-          'es' : 'es-es/',
-          'zh-hans' : 'zh-cn/',
-          'ja' : 'ja-jp/',
-          'ko' : 'ko-kr/'
-        },
-        protocol = location.protocol;
-
-    // An example of loading the menus via AJAX (json).
-    // Prepend the language if appropriate.
-    /*$.ajax({
-      url: protocol + '://' + domain + '/' + lang_map[lang] + 'ajax/megamenu',
-      type: 'GET',
-      dataType: 'json',
-      success: tabAjaxMegaMenu,
-      error: function(xhr, ajaxOptions, error) {
-        console.log(xhr.status);
-        console.log(error);
-      }
-    });*/
-
-    // An example of loading the menus via AJAX (jsonp).
-    // Prepend the language if appropriate.
     $.ajax({
-        url: protocol + '://' + domain + '/' + lang_map[lang] + 'ajax/megamenu' + '/jsonp/' + 'tabAjaxMegaMenu',
+        url: 'https://redesign-tableau.com/ajax/megamenu/jsonp/tabAjaxMegaMenu',
         type: 'POST',
         dataType: 'jsonp'
     });
