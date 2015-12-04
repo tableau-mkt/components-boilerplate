ab765aa83eb5a291073530e8fb83fcfc28ff9c72
BJ Miller <bjmiller@tableausoftware.com>
2015-12-04 14:19:27 -0800

    Fixing the domain and adding basic auth credentials in the demonstration ajax call

--- components/navigation/_ajax_consumption/ajax_consumption.styleguide.js
+++ components/navigation/_ajax_consumption/ajax_consumption.styleguide.js
@@ -1,7 +1,7 @@
 (function($) {
   $(document).ready(function() {
     $.ajax({
-        url: 'https://redesign-tableau.com/ajax/megamenu/jsonp/tabAjaxMegaMenu',
+        url: 'https://give:feedback@redesign-tableau.gotpantheon.com/ajax/megamenu/jsonp/tabAjaxMegaMenu',
         type: 'POST',
         dataType: 'jsonp'
     });
