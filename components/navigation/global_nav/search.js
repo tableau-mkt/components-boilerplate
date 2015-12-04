7aab4095a5c162b5f829d5b3122559d9b5f6ccaf
Gary Gao <ggao@tableausoftware.com>
2015-12-04 13:42:21 -0800

    Revert "Move globa-nav search styleguide specific js"
    This reverts commit f18de52d8904f584bf76bb7d0c4ef4416d1218bb.

--- components/navigation/global_nav/search.js
+++ components/navigation/global_nav/search.js
@@ -1,10 +1,24 @@
-/**
+/** 
  * Global search bar interaction
  */
-(function($) {
+(function($){
   $(document).ready(function(){
     var $search = $('.global-nav__search'),
-        $closeSearch = $search.find('.global-nav__search__close');
+        $closeSearch = $search.find('.global-nav__search__close'),
+        availableTags = [
+          "Tableau",
+          "Desktop",
+          "Server",
+          "Online",
+          "Cloud",
+          "Public",
+          "Reader",
+          "Business Intelligence",
+          "Data",
+          "Products",
+          "Graphs",
+          "Visualizations"
+        ];
 
     $search.on('click', function(e){
       e.preventDefault();
@@ -16,5 +30,14 @@
       e.preventDefault();
       $search.parents('.global-nav__top').removeClass('global-nav--search-shown');
     });
+    
+    // Search auto-complete for demo purposes. Requires jQuery UI Autocomplete
+    // @todo add support for highlighting the searched characters in the list
+    //    http://stackoverflow.com/questions/2435964/jqueryui-how-can-i-custom-format-the-autocomplete-plug-in-results
+    $search.find("input").autocomplete({
+      source: availableTags,
+      appendTo: ".global-nav"
+    });
+
   });
 })(jQuery);
