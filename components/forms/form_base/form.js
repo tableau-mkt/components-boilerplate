3b0a8aec2886d533462d85fce1f00919139e5ed3
Joel Walters <jwalters@tableau.com>
2015-12-28 18:21:05 -0800

    Refactor form.js to expose init function, and respond to custom initFloatLabels event.

--- components/forms/form_base/form.js
+++ components/forms/form_base/form.js
@@ -1,10 +1,22 @@
-$(document).ready(function () {
-  var options = {
-    labelSelector: '.form-field__label'
-  };
+// Loose augmentation pattern. Creates top-level Tabia variable if it doesn't
+// already exist.
+var Tabia = Tabia || {};
 
-  $('.has-float-label').find('input, select, textarea')
+Tabia.form = {};
+
+Tabia.form.initFloatLabels = function ($elements) {
+  $elements.find('input, select, textarea')
     .not('[type="checkbox"], [type="radio"]')
     .closest('.form-field')
-    .floatLabel(options);
+    .floatLabel({
+      labelSelector: '.form-field__label'
+    });
+};
+
+$(document).ready(function () {
+  Tabia.form.initFloatLabels($('.has-float-label'));
+});
+
+$(document).on('initFloatLabels', function (e) {
+  Tabia.form.initFloatLabels($(e.target));
 });
