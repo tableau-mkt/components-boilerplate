d196819069dc4001fd4219fe1b50d83d6beac6be
Gary Gao <ggao@tableausoftware.com>
2016-01-11 12:52:23 -0800

    Add has-suggestion state handler; additional unbinds when form is submitted

--- components/forms/content_search/content-search.js
+++ components/forms/content_search/content-search.js
@@ -58,7 +58,8 @@ Tabia.contentSearch.resetForm = function($search) {
  */
 Tabia.contentSearch.submitForm = function($search) {
   if ($search.find('.content-search__input').val() !== '') {
-    $search.addClass('is-populated');
+    $search.removeClass('has-suggestion')
+      .addClass('is-populated')
     $search.find('.content-search__submit').click();
   }
 };
@@ -74,10 +75,14 @@ Tabia.contentSearch.keydownHandler = function (event) {
 
   switch (event.which) {
     case 13: // ENTER
+      $search.removeClass('has-suggestion');
       // Allow overriding.
       $(document).trigger($submitEvent);
       // Submit the form, via AJAX.
       if (!$submitEvent.isDefaultPrevented()) {
+        // Prevent any further events from occurring on the input.
+        $search.find('.content-search__input').prop('readonly', true)
+          .off('keyup keydown blur');
         Tabia.contentSearch.submitForm($search);
       }
       event.preventDefault();
