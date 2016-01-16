dd5f1dc8bd68c3e0f11a72dfca64320442989fb3
Etroid <mkeereman@tableau.com>
2016-01-16 12:25:53 -0800

    Notification component.

---
+++ components/interactive/notification/notification.js
@@ -0,0 +1,11 @@
+(function($){
+  $(document).ready(function() {
+
+    /**
+     * Handles closing the notification.
+     */
+    $('.notification .notification__close').click(function () {
+      $('.notification').slideUp();
+    });
+  });
+})(jQuery);
