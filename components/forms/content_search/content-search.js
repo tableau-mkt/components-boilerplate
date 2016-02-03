cfa49eadcc56370bd317ec8f4164843ad2deeab4
Joel Walters <jwalters@tableau.com>
2016-02-02 16:12:32 -0800

    Content search: allow re-initialization (e.g. AJAX content in Drupal)
    - New: `Components.contentSearch.initialize($searchElement);`

--- components/forms/content_search/content-search.js
+++ components/forms/content_search/content-search.js
@@ -1,7 +1,5 @@
 /**
- * Simple content search behaviors.
- *
- * - Handle down/up arrow keys on pick list
+ * Content search behaviors.
  */
 
 // Loose augmentation pattern. Creates top-level Components variable if it
@@ -11,82 +9,95 @@ var Components = Components || {};
 // Create a base for this module's data and functions.
 Components.contentSearch = {};
 
-/**
- * DOM-ready callback.
- *
- * @param {Object} $
- *   jQuery
- */
-Components.contentSearch.ready = function ($) {
-  // Set up all the section search components on the page.
-  $('.content-search').not('.contextual-search').each(function () {
-    var $this = $(this);
+// Closure to extend behavior, provide privacy and state.
+(function (component, $) {
 
+  /**
+   * DOM-ready callback.
+   *
+   * @param {Object} $
+   *   jQuery
+   */
+  component.ready = function ($) {
+    // Set up all the section search components on the page.
+    $('.content-search').not('.contextual-search').each(function () {
+      component.initialize($(this));
+    });
+  };
+
+  /**
+   * Initialize component.
+   * - Binds contentSearch event handlers, and unbinds any existing ones.
+   */
+  component.initialize = function ($search) {
     // Attach keydown handler with context.
-    $this.find('.content-search__input').keydown(
-      $.proxy(Components.contentSearch.keydownHandler, $this)
+    $search.find('.content-search__input')
+      .off('keydown.contentSearch')
+      .on('keydown.contentSearch', $.proxy(Components.contentSearch.keydownHandler, $search)
     );
 
-    // Attach reset handler.
-    $this.find('.content-search__reset').click(function (event) {
-      // Allow overriding.
-      var $resetEvent = $.Event('contentSearch:reset');
-      $this.trigger($resetEvent);
-      if (!$resetEvent.isDefaultPrevented()) {
-        // Reset/empty the form, via AJAX.
-        Components.contentSearch.resetForm($this);
-      }
-    });
-  });
-};
+    // Attach reset click handler to component.
+    $search.find('.content-search__reset')
+      .off('click.contentSearch')
+      .on('click.contentSearch', function () {
+        // Allow overriding.
+        var resetEvent = $.Event('contentSearch:reset');
+        $search.trigger(resetEvent);
+        if (!resetEvent.isDefaultPrevented()) {
+          // Reset/empty the form, via AJAX.
+          component.resetForm($search);
+        }
+      });
+  };
 
-/**
- * Carry out the form reset.
- *
- * @param {jQuery Object} $search
- */
-Components.contentSearch.resetForm = function($search) {
-  $search.removeClass('has-suggestion');
-  $search.find('.content-search__input').val('');
-};
-
-/**
- * Carry out the form submit.
- *
- * @param {jQuery Object} $search
- */
-Components.contentSearch.submitForm = function($search) {
-  if ($search.find('.content-search__input').val() !== '') {
+  /**
+   * Carry out the form reset.
+   *
+   * @param {jQuery Object} $search
+   */
+  component.resetForm = function ($search) {
     $search.removeClass('has-suggestion');
-    $search.find('.content-search__submit').click();
-  }
-};
+    $search.find('.content-search__input').val('');
+  };
 
-/**
- * Keydown handler.
- *
- * @param {Object} event
- */
-Components.contentSearch.keydownHandler = function (event) {
-  var $search = $(this[0]),
-      $submitEvent = $.Event('contentSearch:submit');
+  /**
+   * Carry out the form submit.
+   *
+   * @param {jQuery Object} $search
+   */
+  component.submitForm = function ($search) {
+    if ($search.find('.content-search__input').val() !== '') {
+      $search.addClass('has-suggestion');
+      $search.find('.content-search__submit').click();
+    }
+  };
 
-  switch (event.which) {
-    case 13: // ENTER
-      $search.removeClass('has-suggestion');
-      // Allow overriding.
-      $(document).trigger($submitEvent);
-      // Submit the form, via AJAX.
-      if (!$submitEvent.isDefaultPrevented()) {
-        // Prevent any further events from occurring on the input.
-        $search.find('.content-search__input').prop('readonly', true)
-          .off('keyup keydown blur');
-        Components.contentSearch.submitForm($search);
-      }
-      event.preventDefault();
-      break;
-  }
-};
+  /**
+   * Keydown handler.
+   *
+   * @param {Object} event
+   */
+  component.keydownHandler = function (event) {
+    var $search = $(this[0]),
+        submitEvent = $.Event('contentSearch:submit');
+
+    switch (event.which) {
+      case 13: // ENTER
+        // Allow overriding.
+        $search.trigger(submitEvent);
+        // Submit the form, via AJAX.
+        if (!submitEvent.isDefaultPrevented()) {
+          // Prevent any further events from occurring on the input.
+          $search.find('.content-search__input').prop('readonly', true)
+            .off('keyup keydown blur');
+          Components.contentSearch.submitForm($search);
+        }
+        event.preventDefault();
+        break;
+    }
+  };
+
+})(Components.contentSearch, jQuery);
 
 // Attach our DOM-ready callback.
 jQuery(Components.contentSearch.ready);
