cd0ca8d62e9cce6b06bfb329ae2457f95948107b
BJ Miller <bjmiller@tableau.com>
2016-05-09 09:15:18 -0500

    Updating accordion

--- components/interactive/accordion/accordion.js
+++ components/interactive/accordion/accordion.js
@@ -4,23 +4,27 @@
 
 (function($){
   $(document).ready(function(){
-    var $accordion = $('.accordion');
+    var $accordion = $('.accordion'),
+        animation = {
+          duration: 400,
+          easing: "easeInOutQuart"
+        };
 
     if (!$accordion.length) {
       return;
     }
 
-    $('.accordion .accordion__content-wrapper').not('.open .accordion__content-wrapper').hide();
-    $('.accordion .accordion__title-wrapper').click( function(e) {
+    $('.accordion .accordion__content').not('.open .accordion__content').hide();
+    $('.accordion .accordion__title').click( function(e) {
       var $this = $(this),
           $openItems = $this.parent().siblings('.open');
 
       // Close other open items.
-      $openItems.find('.accordion__content-wrapper').slideToggle(250, 'linear');
+      $openItems.find('.accordion__content').slideToggle(animation);
       $openItems.toggleClass('open');
 
       // Open new item.
-      $this.siblings('.accordion__content-wrapper').slideToggle(250, 'linear');
+      $this.siblings('.accordion__content').slideToggle(animation);
       $this.parents('.accordion__item').toggleClass('open');
 
       e.preventDefault();
@@ -29,7 +33,7 @@
     // Auto-scroll and expand accordions when linked to with a hash
     var hash = window.location.hash;
     if ($(hash).length && $(hash).closest('.accordion__item').length) {
-      $(hash).parents('.accordion__title-wrapper').trigger('click');
+      $(hash).parents('.accordion__title').trigger('click');
     }
   });
 })(jQuery);
